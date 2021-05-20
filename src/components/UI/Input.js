ddb7f216facdaeeb0ada6b8dcd43f2176e4575ef
asad0010 <asad0010@algonquinlive.com>
2021-05-20 15:43:27 -0400

    Added a Form in the menu to be able to select amount to add to cart

---
+++ src/components/UI/Input.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './Input.module.css';
+
+const Input = (props) => {
+    return(
+        <div>
+            <label htmlFor={props.input.id}>
+                {props.label}
+            </label>
+            <input {...props.input}/>
+        </div>
+    );
+};
+
+export default Input;
