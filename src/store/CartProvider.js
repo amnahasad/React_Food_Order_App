73042c265879550e3673c93bdb98c6eda039040f
amnahasad <amnah_asad@hotmail.com>
2021-05-26 15:06:10 -0400

    Added a reducer for the cart to make an item appear only once in the cart list

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,10 +8,28 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
     if(action.type === 'ADD_ITEM') {
-        const updatedItems = state.items.concat(action.item);
         const uptdatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
-        return {items: updatedItems,
-        totalAmount: uptdatedTotalAmount};
+
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
+
+        const existingCartItem = state.items[existingCartItemIndex];
+        
+        let updatedItems;
+
+        if(existingCartItem) {
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+            updatedItems = state.items.concat(action.item);
+        }
+
+        return {
+            items: updatedItems,
+            totalAmount: uptdatedTotalAmount};
     }
     return defaultCartState;
 };
